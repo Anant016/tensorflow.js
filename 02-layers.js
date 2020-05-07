fff8a4db0d200223fc7f18690376ae1d8be11684
Anant Rungta <anantrungta1999@gmail.com>
2020-05-07 15:32:39 +0530

    first commit

--- 02-layers.js
+++ 02-layers.js
@@ -1,3 +1,4 @@
+//1.
 const model = tf.sequential();
 
 const hidden = tf.layers.dense({
@@ -22,5 +23,28 @@ model.compile({
 });
 
 //send data
+const xs = tf.tensor2d([
+  [0, 0],
+  [0.5, 0.5],
+  [1, 1],
+]);
+const ys = tf.tensor2d([
+  [1, 2.2, 0],
+  [2, 3, 3],
+  [1, 2, 4],
+]);
+// const ys = tf.tensor2d([[1], [0.5], [0]]);
+
 //train- fit
-//predict
+async function train() {
+  for (let i = 0; i < 10; i++) {
+    const res = await model.fit(xs, ys, { shuffle: true, epochs: 10 });
+    console.log(res.history.loss[0]);
+  }
+}
+
+train().then(() => {
+  console.log("training Completed");
+  //predict
+  model.predict(xs).print();
+});
